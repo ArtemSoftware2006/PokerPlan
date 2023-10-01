fb1bc6f9aae90d85c4e232b9b6619a11cca866aa
Artem <artem.egorov.art2006@gmail.com>
2023-10-01 12:24:46 +0300

    Создал хелпер для отображения карточек голосов

---
+++ CodeCup/Components/CardsDisplayer.cs
@@ -0,0 +1,36 @@
+
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Новая_папка.Components
+{   
+    [ViewComponent]
+    public static class CardsDisplayer
+    {
+        static private TagBuilder cardCreater;
+        public static HtmlString  Invoke(string[] values)
+        {
+            cardCreater = new TagBuilder("div");
+            cardCreater.Attributes.Add("class", "d-flex justify-content-between");
+
+            foreach (var item in values)
+            {
+                TagBuilder card = new TagBuilder("div");
+
+                card.InnerHtml.Append(item);
+                card.Attributes.Add("class", "card--vote m-1");
+
+                cardCreater.InnerHtml.AppendHtml(card);
+            }
+
+            using var writer = new StringWriter();
+            cardCreater.WriteTo(writer, HtmlEncoder.Default);
+
+            return new HtmlString(writer.ToString());
+             
+        }
+    }
+}
